3caef09dba85e90e16cabaf0e87854e9946c198b
aamanlamba <aamanlamba@gmail.com>
2025-09-27 08:03:19 -0400

    FastAPI with request body

--- fastapi1.py
+++ fastapi1.py
@@ -52,3 +52,20 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+# request body
+from pydantic import BaseModel
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    # request body is read as JSON and converted to a Pydantic model
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
