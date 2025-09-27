04c1a670097caa4af9ac2b5f2d6f0cf8fd46730c
aamanlamba <aamanlamba@gmail.com>
2025-09-27 09:50:16 -0400

    validation of query parameters

---
+++ fastapi_queryannotations.py
@@ -0,0 +1,54 @@
+# Query annotations and validations
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+# items endpoint with query parameter validation
+# str is an optional query parameter with max length of 50
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, 
+                                  Query(title="query parameter",
+                                        description="Query string for the items to search in the database that have a good match",
+                                        min_length=3, 
+                                        max_length=50,
+                                        deprecated=True,)] = None,
+                                        ):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    try:
+        if q:
+            results.update({"q": q})
+    except Exception as e:
+        results.update({"error": str(e)})
+    return results
+
+# custom validation using pydaantic AfterValidator
+from pydantic import AfterValidator
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+# books endpoint with custom validation
+import random
+@app.get("/books/")
+async def read_book_items(
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item}
