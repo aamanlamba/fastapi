37253802428afe81871cc390445aba75dfa7cdc2
aamanlamba <aamanlamba@gmail.com>
2025-09-26 22:22:20 -0400

    FastAPI with path parameters

--- fastapi1.py
+++ fastapi1.py
@@ -1,1 +1,33 @@
 # first fastapi sample
+from enum import Enum
+from fastapi import FastAPI
+app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+    
+# root endpoint from fastapi - returns hello world
+@app.get("/")
+async def root():   
+    return {"message": "Hello from FastAPI"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
